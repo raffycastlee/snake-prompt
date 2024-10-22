cdbbe992da903f5be9c2568747218de8f2adde7a
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-22 13:27:32 -0400

    fixes random food points logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const mainGrid = document.getElementById("main-box");
 let currentSquare = document.querySelector("food");
+let prevX = Math.floor(Math.random() * 40);
+let prevY = Math.floor(Math.random() * 40);
 
 const initBoard = () => {
   let rowIndex = 0;
@@ -33,19 +35,27 @@ const initBoard = () => {
 };
 
 const generateApple = () => {
-  const x = Math.floor(Math.random() * 40);
-  const y = Math.floor(Math.random() * 40);
-
+  // makes sure there's no repeat coords
+  let x = Math.floor(Math.random() * 40);
+  let y = Math.floor(Math.random() * 40);
+  while (x === prevX && y === prevY) {
+    x = Math.floor(Math.random() * 40);
+    y = Math.floor(Math.random() * 40);
+  }
   console.log(`setting new food square`);
 
   if (currentSquare !== null) {
     currentSquare.classList.remove("food");
+    currentRow = document.querySelector(`#row-${y}`);
+    currentSquare = currentRow.querySelector(`#column-${x}`);
+    currentSquare.setAttribute("class", "column food");
+  } else {
+    currentRow = document.querySelector(`#row-${y}`);
+    console.log(y);
+    console.log(currentRow);
+    currentSquare = currentRow.querySelector(`#column-${x}`);
+    currentSquare.setAttribute("class", "column food");
   }
-  currentRow = document.querySelector(`#row-${y}`);
-  console.log(currentRow);
-  currentSquare = currentRow.querySelector(`#column-${x}`);
-  console.log(currentSquare);
-  currentSquare.setAttribute("class", "column food");
 };
 
 initBoard();
