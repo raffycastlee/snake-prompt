c4c526d27c60c812a690d0299c12bce6bd10f88f
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-23 14:42:47 -0400

    scoring logic, head movement, out of bounds implemented

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ deployed [here]().inspired by [ben's snake app](https://benspector3.github.io/sn
 - [x] ui boxes
 - [x] food starts at fixed point (10,30)
 - [x] randomize food point
-- [ ] snake movement
+- [x] snake movement
+- [x] food point timer
+- [x] scoring
+- [x] high score per session
 - [ ] snake growing + trail
-- [ ] food point timer
-- [ ] scoring
-- [ ] high score per session
 - [ ] convert prompts and alerts to modals
 - [ ] rainbow snake
 - [ ] maybe make a start menu
--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let prevY = 0;
 // start snake at (10,30)
 let headX = 0;
 let headY = 0;
+// for resetting the food reset interval
+let foodInterval;
+let snakeInterval;
 
 const initBoard = () => {
   // coordinates for squares
@@ -77,63 +80,162 @@ const generateApple = (
 
 // stores snake data
 let playerData = {
-  "head": headSquare,
-  "length": 1,
-  "orientation": null
-}
+  head: [],
+  length: 1,
+  orientation: null,
+  tempOrientation: null,
+  // body is going to be an arr of xy values
+  body: [],
+  currScore: 0,
+  highScore: 0,
+};
 
-const generateSnake = (x,y) => {
+const generateSnake = (x, y) => {
   // base case again
   // again there has to be a better way to do base case
   if (headSquare !== null) {
     headSquare.classList.remove("head");
     currentRow = document.querySelector(`#row-${y}`);
-    headSquare = currentRow.querySelector(`#column-${x}`); 
+    headSquare = currentRow.querySelector(`#column-${x}`);
     // holy shit how do i handle collision
+    // console.log(headSquare.classList[1]);
+    if (headSquare.classList[1] === "food") {
+      updateCurrScore();
+      clearInterval(foodInterval);
+      generateApple();
+      foodInterval = setInterval(generateApple, 5000);
+    }
     headSquare.setAttribute("class", "column head");
+    playerData.head[0] = x;
+    playerData.head[1] = y;
   } else {
     currentRow = document.querySelector(`#row-${y}`);
     headSquare = currentRow.querySelector(`#column-${x}`);
     // holy shit how do i handle collision
     headSquare.setAttribute("class", "column head");
-  }  
-}
+    playerData.head[0] = x;
+    playerData.head[1] = y;
+  }
+};
 
-const moveSnake = (x,y) => {
+const updateCurrScore = () => {
+  document.getElementById(
+    "current-score-box"
+  ).textContent = `current score: ${++playerData.currScore}`;
+};
+
+const updateHighScore = () => {
+  document.getElementById(
+    "current-score-box"
+  ).textContent = `current score: ${playerData.currScore}`;
+  document.getElementById(
+    "high-score-box"
+  ).textContent = `high score: ${playerData.highScore}`;
+};
+
+const moveSnake = () => {
+  playerData.orientation = playerData.tempOrientation;
   // how the fuck do i track the body?
   // can't be just a fade
   // i want to off myself
-  if (playerData.orientation === 'Left') {
-
-  } else if (playerData.orientation === 'Right') {
-
-  } else if (playerData.orientation === 'Down') {
-
-  } else if (playerData.orientation === 'Up') {
-
-  } else { // all other keypresses just ignore
-
+  if (playerData.orientation === "Left") {
+    playerData.head[0]--;
+    // console.log("snake left");
+  } else if (playerData.orientation === "Right") {
+    playerData.head[0]++;
+    // console.log("snake right");
+  } else if (playerData.orientation === "Down") {
+    playerData.head[1]++;
+    // console.log("snake down");
+  } else if (playerData.orientation === "Up") {
+    playerData.head[1]--;
+    // console.log("snake up");
+  } else {
+    // all other keypresses just ignore
+    // console.log("keypress ignored");
   }
-}
+
+  // console.log(playerData.head)
+  try {
+    generateSnake(playerData.head[0], playerData.head[1]);
+  } catch (err) {
+    terminateGame();
+  }
+};
 
 // flag to check if game has already started
 let gameStarted = false;
 // starts game
 onkeydown = (e) => {
-  // removes the 'Arrow'- prefix onkeydown event
-  playerData.orientation = e.key.slice(5);
+  console.log(e.key.slice(5));
+  console.log(gameStarted);
   // makes sure generate doesn't happen more than once naturally
   if (!gameStarted) {
     gameStarted = true;
-    setInterval(generateApple, 1000);
+    foodInterval = setInterval(generateApple, 5000);
   }
-  setInterval(moveSnake, 10);
-}
+  const keypress = e.key.slice(5);
+  if (playerData.orientation === null) {
+    snakeInterval = setInterval(moveSnake, 50);
+    playerData.orientation = keypress;
+    playerData.tempOrientation = keypress;
+    // moveSnake();
+    return;
+  }
+  // do keypress all conditions and stop
+  // if the same as last. and if polar opposite
+  // removes the 'Arrow'- prefix onkeydown event
+  if (keypress === "Left") {
+    if (
+      keypress !== playerData.orientation &&
+      playerData.orientation !== "Right"
+    ) {
+      playerData.tempOrientation = keypress;
+    }
+  } else if (keypress === "Right") {
+    if (
+      keypress !== playerData.orientation &&
+      playerData.orientation !== "Left"
+    ) {
+      playerData.tempOrientation = keypress;
+    }
+  } else if (keypress === "Down") {
+    if (
+      keypress !== playerData.orientation &&
+      playerData.orientation !== "Up"
+    ) {
+      playerData.tempOrientation = keypress;
+    }
+  } else if (keypress === "Up") {
+    if (
+      keypress !== playerData.orientation &&
+      playerData.orientation !== "Down"
+    ) {
+      playerData.tempOrientation = keypress;
+    }
+  }
+  // console.log(playerData);
+};
 
+const terminateGame = () => {
+  prevX = 0;
+  prevY = 0;
+  headX = 0;
+  headY = 0;
+  playerData.orientation = null;
+  playerData.tempOrientation = null;
+  playerData.head.length = 0;
+  playerData.highScore = Math.max(playerData.currScore, playerData.highScore);
+  playerData.currScore = 0;
+  updateHighScore();
+  gameStarted = false;
+  clearInterval(snakeInterval);
+  clearInterval(foodInterval);
+  initBoard();
+  generateApple(10, 30);
+  generateSnake(30, 10);
+};
 
-
-// ------------------ START GAME ------------------ // 
-initBoard();
-generateApple(10, 30);
-generateSnake(30, 10);
+// ------------------ START GAME ------------------ //
+terminateGame();
 // setInterval(generateApple, 10);
